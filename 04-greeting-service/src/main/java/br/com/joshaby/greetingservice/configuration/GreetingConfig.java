43387a6a40fe77969ac5dd361fe3642fc9f4f015
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2022-10-20 23:48:52 -0300

    Adiciona anotação RefreshScope para atualização de configuração

--- 04-greeting-service/src/main/java/br/com/joshaby/greetingservice/configuration/GreetingConfig.java
+++ 04-greeting-service/src/main/java/br/com/joshaby/greetingservice/configuration/GreetingConfig.java
@@ -3,9 +3,11 @@ package br.com.joshaby.greetingservice.configuration;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
+@RefreshScope
 @ConfigurationProperties("greeting-service")
 @Getter
 @Setter
